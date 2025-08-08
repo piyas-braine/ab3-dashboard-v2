f25ba5c04ec1be3794fd86d562964b0efa0c3c78
swapnil-braine <swapnil.braine@gmail.com>
2025-08-08 12:37:54 +0600

    added chat icon in the flow.

--- src/app/setup-organization/page.tsx
+++ src/app/setup-organization/page.tsx
@@ -5,15 +5,16 @@ import SetupOrganizationStep1 from "@/components/setup-organization/SetupOrganiz
 import SetupOrganizationStep2 from "@/components/setup-organization/SetupOrganizationStep2";
 import SetupOrganizationStep3 from "@/components/setup-organization/SetupOrganizationStep3";
 import SetupOrganizationStep4 from "@/components/setup-organization/SetupOrganizationStep4";
+import ChatIcon from "@/components/Svgs/ChatIcon";
 import { useState } from "react";
 
 const SetupOrganizationPage = () => {
   const [stepNumber, setStepNumber] = useState(1);
 
   return (
-    <div>
+    <div className="relative ">
       <div className="flex flex-col lg:flex-row">
-        <div className="flex-[6.58] min-w-0">
+        <div className="flex-[6.58] min-h-screen min-w-0">
           <AuthLeftBanner stepNumber={stepNumber} />
         </div>
 
@@ -32,6 +33,17 @@ const SetupOrganizationPage = () => {
         {stepNumber === 4 && (
           <SetupOrganizationStep4 setStepNumber={setStepNumber} />
         )}
+      </div>
+
+      <div
+        className="absolute bottom-[31px] right-[34px] w-[80px] h-[80px] rounded-full flex justify-center items-center"
+        style={{
+          background: `linear-gradient(225deg, #29272E 0%, #27272E 100%)`,
+        }}
+      >
+        <div className="w-[27.23px] h-[27.23px]">
+          <ChatIcon />
+        </div>
       </div>
     </div>
   );
--- src/components/setup-organization/AuthLeftBanner.tsx
+++ src/components/setup-organization/AuthLeftBanner.tsx
@@ -15,7 +15,7 @@ const AuthLeftBanner = ({ stepNumber }: { stepNumber: number }) => {
   }, [stepNumber]);
 
   return (
-    <div className="pt-[33.5px] pb-[57px] px-4 sm:px-7 xl:px-[60px] min-[1400px]:pl-[83px] min-[1400px]:pr-[102px] bg-[#F7FAFC]">
+    <div className="pt-[33.5px] pb-[57px] px-4 sm:px-7 xl:px-[60px] min-[1400px]:pl-[83px] min-[1400px]:pr-[102px] h-full flex items-center bg-[#F7FAFC]">
       <div className="max-w-[513px] mx-auto w-full flex flex-col items-center">
         <div className="w-[135px] h-[84.28px]">
           <Image
@@ -27,9 +27,23 @@ const AuthLeftBanner = ({ stepNumber }: { stepNumber: number }) => {
           />
         </div>
 
-        <h1 className="mt-[68.22px] text-text-primary-cetacean-blue text-[28px] leading-[100%] font-bold text-center">
-          Welcome to AB3 <span className="text-text-primary-red">Medical</span>
-        </h1>
+        <div className="mt-[68.22px] flex justify-center items-center">
+          <h2 className="text-text-primary-cetacean-blue text-[28px] leading-[100%] font-bold text-center">
+            Welcome to
+          </h2>
+
+          <Image
+            src={ab3MedicalLogo}
+            alt="AB3 Medical"
+            width={43.37}
+            height={27.07}
+            className="w-[43.37px] h-[27.07px] ml-[6.29px] mr-[6.34px]"
+          />
+
+          <h2 className=" text-text-primary-red text-[28px] leading-[100%] font-bold text-center">
+            Medical
+          </h2>
+        </div>
 
         <div className="mt-[96px] relative z-10 w-full">
           <div className="relative pt-[21px] pb-5 pl-[19px] pr-[18px] w-full rounded-[22px] bg-bg-surface-primary/50">
@@ -102,12 +116,10 @@ const AuthLeftBanner = ({ stepNumber }: { stepNumber: number }) => {
             <div className="hidden sm:block absolute -z-20 left-1/2 -translate-x-1/2 -top-[35px] -bottom-[35px] w-full h-full sm:w-[421px] sm:h-[421px] bg-bg-primary-blue rounded-full"></div>
           </div>
         </div>
-
         <p className="mt-[128px] max-w-[447px] mx-auto text-[18px] leading-[30px] font-medium text-text-body-light text-center">
           Let’s see what we have new, check it out! So maybe explore here
           something more.
         </p>
-
         <div className="mt-[22px] w-[191px] h-[8px] bg-bg-default-danger-soft  rounded-xl">
           <div
             style={{ width: `${currentProgress}%` }}
