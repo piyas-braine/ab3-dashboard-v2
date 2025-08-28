fd120f78b2b0a29bab2cdb56afa44cee1b3f5d24
swapnil-braine <swapnil.braine@gmail.com>
2025-08-29 00:17:17 +0600

    Added Select Label dropdown. Made the add note modal responsive.

---
+++ src/data/noteLabels.ts
@@ -0,0 +1,18 @@
+export const noteLabels = [
+    { id: "1", label: "Adult Health", value: "Adult Health", }, // purple
+    { id: "2", label: "Treatment", value: "Treatment", priority: "Urgent" }, // red
+    { id: "3", label: "Assessment", value: "Assessment", priority: "Normal" }, // green
+    { id: "4", label: "ECHO", value: "ECHO", priority: "Routine" }, // purple
+    { id: "5", label: "ECG", value: "ECG", priority: "Normal" }, // green
+    { id: "6", label: "Anti-Dopping", value: "Anti-Dopping", priority: "Important" }, // yellow
+    { id: "7", label: "Consent", value: "Consent", priority: "Important" }, // yellow
+    { id: "8", label: "Case Review", value: "Case Review", priority: "Routine" }, // purple
+    { id: "9", label: "X-Ray", value: "X-Ray", priority: "Important" }, // yellow
+    { id: "10", label: "Bloods", value: "Bloods", priority: "Urgent" }, // red
+    { id: "11", label: "Cardiac Screen", value: "Cardiac Screen", priority: "Normal" }, // green
+    { id: "12", label: "Dental", value: "Dental" }, // purple
+    { id: "13", label: "Female Health", value: "Female Health", priority: "Routine" }, // purple
+    { id: "14", label: "Emergency", value: "Emergency", priority: "Urgent" }, // red
+    { id: "15", label: "Male Health", value: "Male Health", priority: "Routine" }, // purple
+    { id: "16", label: "ECG", value: "ECG", priority: "Urgent" }, // red
+]
